fe954308867d8869d022849a62079166a41c06e2
Phi Tran <phitran@Phis-MacBook-Pro.local>
2019-03-12 18:36:55 -0500

    Updates

---
+++ precache-manifest.491bb0618eac3bfe90eb69f513b832e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "06824c716f9a9b3d64cc",
+    "url": "/Clicky-Game/static/js/main.41d3156c.chunk.js"
+  },
+  {
+    "revision": "a49af3237dbddbb747fb",
+    "url": "/Clicky-Game/static/js/2.650383ae.chunk.js"
+  },
+  {
+    "revision": "06824c716f9a9b3d64cc",
+    "url": "/Clicky-Game/static/css/main.c750a9a9.chunk.css"
+  },
+  {
+    "revision": "43bd458f6a2dd5d244d1bfff99840125",
+    "url": "/Clicky-Game/index.html"
+  }
+];
